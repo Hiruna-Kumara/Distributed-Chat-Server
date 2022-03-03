65d4e98a8b3d0a2af5cdb1387267a44b43a936d0
kashyapa_niyarepola <kashyapaniyarepola@gmail.com>
2022-03-03 14:58:56 +0530

    new identity validation and message passing to the client

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,94 +1,105 @@
 package server;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.*;
+import java.nio.charset.StandardCharsets;
 
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
 
-// import org.json.simple.JSONObject;
+public class Server implements Runnable{
 
-class Server {
+    private DataOutputStream out;
+    private ServerSocket Server;
 
-    private static boolean hasKey(JSONObject jsonObject, String key) {
+    private boolean hasKey(JSONObject jsonObject, String key) {
         return (jsonObject != null && jsonObject.get(key) != null);
     }
 
-    private static boolean checkID(String id) {
+    private boolean checkID(String id) {
         return (Character.toString(id.charAt(0)).matches("[a-zA-Z]+") && id.matches("[a-zA-Z0-9]+") && id.length() >= 3 && id.length() <= 16);
     }
-    public static void main(String argv[]) throws Exception {
+
+    private void send(JSONObject obj) throws IOException {
+        System.out.println(obj.toJSONString()+"\n");
+        out.write((obj.toJSONString() + "\n").getBytes("UTF-8"));
+        out.flush();
+    }
+
+    private void messageSend(Socket socket, String approve) throws IOException {
+        JSONObject sendToClient = new JSONObject();
+        sendToClient = Massege.getApprovalNewID(approve);
+        System.out.println(sendToClient);
+        send(sendToClient);
+    }
+
+    @Override
+    public void run() {
         String fromclient;
-        // String toclient;
 
-        ServerSocket Server = new ServerSocket(5000);
-        System.out.println(Server.getInetAddress());
-        System.out.println(Server.getLocalSocketAddress());
-        System.out.println(Server.getLocalPort());
+        try {
+            Server = new ServerSocket(5000);
+            System.out.println(Server.getInetAddress());
+            System.out.println(Server.getLocalSocketAddress());
+            System.out.println(Server.getLocalPort());
 
-        System.out.println("TCPServer Waiting for client on port 5000"); //client should use 5000 as port
+            System.out.println("TCPServer Waiting for client on port 5000"); //client should use 5000 as port
 
-        while (true) {
-            Socket connected = Server.accept();
-            System.out.println(" THE CLIENT" + " " + connected.getInetAddress()
-                    + ":" + connected.getPort() + " IS CONNECTED ");
+            while (true) {
+                Socket connected = Server.accept();
+                System.out.println(" THE CLIENT" + " " + connected.getInetAddress()
+                        + ":" + connected.getPort() + " IS CONNECTED ");
 
-            // BufferedReader inFromUser = new BufferedReader(
-            //         new InputStreamReader(System.in));
+                BufferedReader inFromClient = new BufferedReader(
+                        new InputStreamReader(connected.getInputStream(), StandardCharsets.UTF_8));
 
-            BufferedReader inFromClient = new BufferedReader(
-                    new InputStreamReader(connected.getInputStream()));
+                out = new DataOutputStream(connected.getOutputStream());
 
-            // PrintWriter outToClient = new PrintWriter(
-            //         connected.getOutputStream(), true);
+                boolean close = false;
 
-            // while (true) {
+                while (!close) {
 
-            //     System.out.println("SEND(Type Q or q to Quit):");
-            //     toclient = inFromUser.readLine();
+                    fromclient = inFromClient.readLine();
 
-            //     if (toclient.equals("q") || toclient.equals("Q")) {
-            //         outToClient.println(toclient);
-            //         connected.close();
-            //         break;
-            //     } else {
-            //         outToClient.println(toclient);
-            //     }
-            boolean close = false;
-            while (!close) {
+                    //convert received message to json object
 
-                fromclient = inFromClient.readLine();
+                    try {
+                        Object object = null;
+                        JSONParser jsonParser = new JSONParser();
+                        object = jsonParser.parse(fromclient);
+                        JSONObject j_object = (JSONObject) object;
 
-                // if (fromclient.equals("q") || fromclient.equals("Q")) {
-                //     connected.close();
-                //     break;
-                Object object = null;
-                JSONParser jsonParser = new JSONParser();
-                object = jsonParser.parse(fromclient);
-                JSONObject j_object = (JSONObject) object;
-
-                if (hasKey(j_object, "type")) {
-                    //check new identity format
-                    if (j_object.get("type").equals("newidentity") && j_object.get("identity") != null) {
-                        String id = j_object.get("identity").toString();
-                        if (checkID(id)) {
-                            System.out.println("Recieved correct ID type::" + fromclient);
+                        if (hasKey(j_object, "type")) {
+                            //check new identity format
+                            if (j_object.get("type").equals("newidentity") && j_object.get("identity") != null) {
+                                String id = j_object.get("identity").toString();
+                                if (checkID(id)) {
+                                    System.out.println("Recieved correct ID type::" + fromclient);
+                                    messageSend(connected,"true");
+                                } else {
+                                    System.out.println("Recieved wrong ID type:: The identity must be an alphanumeric string starting with an upper or lower case character. It must be at least 3 characters and no more than 16 characters long.");
+                                    messageSend(connected,"false");
+                                }
+                            }
                         } else {
-                            System.out.println("Recieved wrong ID type:: The identity must be an alphanumeric string starting with an upper or lower case character. It must be at least 3 characters and no more than 16 characters long.");
+                            System.out.println("Something went wrong");
                         }
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
                     }
-                } else {
-                    System.out.println("Something went wrong");
                 }
+                connected.close();
 
             }
-            connected.close();
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
