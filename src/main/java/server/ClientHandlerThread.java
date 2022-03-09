213d690c442b828a76f4aadd7ac5eccb2f9834c5
kashyapa_niyarepola <kashyapaniyarepola@gmail.com>
2022-03-09 10:02:21 +0530

    file content changes

--- src/main/java/server/ClientHandlerThread.java
+++ src/main/java/server/ClientHandlerThread.java
@@ -12,13 +12,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Arrays;
 
+import static messaging.MessageTransfer.*;
+
 public class ClientHandlerThread extends Thread {
 
     private final Socket clientSocket;
     private ClientState clientState;
 
-    // TODO : check input stream local var
-    private DataOutputStream dataOutputStream;
 
     public ClientHandlerThread(Socket clientSocket) {
         String serverID = ServerState.getInstance().getServerID();
@@ -27,32 +27,6 @@ public class ClientHandlerThread extends Thread {
         this.clientSocket = clientSocket;
     }
 
-    // check the existence of a key in json object
-    private boolean hasKey(JSONObject jsonObject, String key) {
-        return (jsonObject != null && jsonObject.get(key) != null);
-    }
-
-    // check validity of the ID
-    private boolean checkID(String id) {
-        return (Character.toString(id.charAt(0)).matches("[a-zA-Z]+") && id.matches("[a-zA-Z0-9]+") && id.length() >= 3
-                && id.length() <= 16);
-    }
-
-    // send broadcast message
-    private void sendBroadcast(JSONObject obj, ArrayList<Socket> socketList) throws IOException {
-        for (Socket each : socketList) {
-            Socket TEMP_SOCK = (Socket) each;
-            PrintWriter TEMP_OUT = new PrintWriter(TEMP_SOCK.getOutputStream());
-            TEMP_OUT.println(obj);
-            TEMP_OUT.flush();
-        }
-    }
-
-    // send message to client
-    private void send(JSONObject obj) throws IOException {
-        dataOutputStream.write((obj.toJSONString() + "\n").getBytes(StandardCharsets.UTF_8));
-        dataOutputStream.flush();
-    }
 
     // format message before sending it to client
     private void messageSend(ArrayList<Socket> socketList, String msg, List<String> msgList) throws IOException {
@@ -60,25 +34,25 @@ public class ClientHandlerThread extends Thread {
         String[] array = msg.split(" ");
         if (array[0].equals("newid")) {
             sendToClient = ServerMessage.getApprovalNewID(array[1]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomchange")) {
             sendToClient = ServerMessage.getJoinRoom(array[1], array[2].replace("_", ""), array[3]);
             sendBroadcast(sendToClient, socketList);
         } else if (array[0].equals("createroom")) {
             sendToClient = ServerMessage.getCreateRoom(array[1], array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomchangeall")) {
             sendToClient = ServerMessage.getCreateRoomChange(array[1], array[2], array[3]);
             sendBroadcast(sendToClient, socketList);
         } else if (array[0].equals("roomcontents")) {
             sendToClient = ServerMessage.getWho(array[1], msgList, array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomlist")) {
             sendToClient = ServerMessage.getList(msgList);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("deleteroom")) {
             sendToClient = ServerMessage.getDeleteRoom(array[1], array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("message")) {
             sendToClient = ServerMessage.getMessage(array[1],
                     String.join(" ", Arrays.copyOfRange(array, 2, array.length)));
@@ -342,7 +316,6 @@ public class ClientHandlerThread extends Thread {
             BufferedReader bufferedReader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
 
-            this.dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
             while (true) {
 
