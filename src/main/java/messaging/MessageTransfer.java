297c5a9ab81eca29aa77563cdc0dc57642d2393f
malithbc <malithattanayaka99@gmail.com>
2022-03-13 00:05:41 -0800

    modify messagetransfer

--- src/main/java/messaging/MessageTransfer.java
+++ src/main/java/messaging/MessageTransfer.java
@@ -1,10 +1,10 @@
 package messaging;
 
+import consensus.LeaderState;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import server.Server;
-import consensus.LeaderState;
 import server.ServerState;
 
 import java.io.DataOutputStream;
@@ -14,21 +14,24 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-public class MessageTransfer {
-    public static JSONObject convertToJson(String jsonString) {
+public class MessageTransfer
+{
+    public static JSONObject convertToJson(String jsonString)
+    {
         JSONObject j_object = null;
-        try {
+        try
+        {
             JSONParser jsonParser = new JSONParser();
             Object object = jsonParser.parse(jsonString);
             j_object = (JSONObject) object;
 
-        } catch (ParseException e) {
+        } catch(ParseException  e) {
             e.printStackTrace();
         }
         return j_object;
     }
 
-    // check the existence of a key in json object
+    //check the existence of a key in json object
     public static boolean hasKey(JSONObject jsonObject, String key) {
         return (jsonObject != null && jsonObject.get(key) != null);
     }
@@ -37,7 +40,6 @@ public class MessageTransfer {
     public static boolean checkID(String id) {
         return (Character.toString(id.charAt(0)).matches("[a-zA-Z]+")
                 && id.matches("[a-zA-Z0-9]+") && id.length() >= 3 && id.length() <= 16);
-
     }
 
     //send broadcast message
@@ -56,21 +58,9 @@ public class MessageTransfer {
         dataOutputStream.flush();
     }
 
-
     //send message to server
-    public static void sendServer( JSONObject obj, Server destServer) throws IOException{
-
-    Socket socket = new Socket(destServer.getServerAddress(),
-                destServer.getCoordinationPort());
-        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-        dataOutputStream.write((obj.toJSONString() + "\n").getBytes(StandardCharsets.UTF_8));
-        dataOutputStream.flush();
-    }
-
-    //send message to leader server
-    public static void sendToLeader(JSONObject obj) throws IOException {
-        Server destServer = ServerState.getInstance().getServers()
-                .get( LeaderState.getInstance().getLeaderID() );
+    public static void sendServer( JSONObject obj, Server destServer) throws IOException
+    {
         Socket socket = new Socket(destServer.getServerAddress(),
                 destServer.getCoordinationPort());
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
@@ -86,10 +76,13 @@ public class MessageTransfer {
             dataOutputStream.write((obj.toJSONString() + "\n").getBytes( StandardCharsets.UTF_8));
             dataOutputStream.flush();
         }
-
     }
 
-    public static void sendRooms(JSONObject obj, Server destServer) throws IOException {
+    //send message to leader server
+    public static void sendToLeader(JSONObject obj) throws IOException
+    {
+        Server destServer = ServerState.getInstance().getServers()
+                .get( LeaderState.getInstance().getLeaderID() );
         Socket socket = new Socket(destServer.getServerAddress(),
                 destServer.getCoordinationPort());
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
